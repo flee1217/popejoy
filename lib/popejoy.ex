190ddd56095fab44435dc28866ebb3a1991a50ed
flee1217 <flee1217@unm.edu>
2022-11-02 21:49:51 -0600

    add project files

---
+++ lib/popejoy.ex
@@ -0,0 +1,18 @@
+defmodule Popejoy do
+  @moduledoc """
+  Documentation for Popejoy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Popejoy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/popejoy_test.exs
@@ -0,0 +1,8 @@
+defmodule PopejoyTest do
+  use ExUnit.Case
+  doctest Popejoy
+
+  test "greets the world" do
+    assert Popejoy.hello() == :world
+  end
+end
